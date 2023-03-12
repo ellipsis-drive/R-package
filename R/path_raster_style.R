e7203ccd42ac051fdebf4214fe7f504baf9b3d89
B-Starkenburg <berendstarkenburg@gmail.com>
2023-03-12 15:31:45 +0100

    Added path.raster.style

---
+++ R/path_raster_style.R
@@ -0,0 +1,37 @@
+#' @export
+path.raster.style.add <- function(pathId, name, method, parameters, token, default = TRUE)
+{
+  token <- validString("token", token, TRUE)
+  pathId <- validUuid("pathId", pathId, TRUE)
+  method <- validString("method", method, TRUE)
+  name <- validString("name", name, TRUE)
+  default <- validBool("default", default, TRUE)
+  parameters <- validObject("parameters", parameters, TRUE)
+
+  body = list("name" = name, "method" = method, "parameters" = parameters, "default" = default)
+  return(apiManager_post(glue::glue("/path/{pathId}/raster/style"), body, token))
+}
+
+#' @export
+path.raster.style.delete <- function(pathId, styleId, token)
+{
+  token <- validString("token", token, TRUE)
+  pathId <- validUuid("pathId", pathId, TRUE)
+  styleId <- validUuid("styleId", styleId, TRUE)
+
+  return(apiManager_delete(glue::glue("/path/{pathId}/raster/style/{styleId}"), NULL, token))
+}
+
+#' @export
+path.raster.style.edit <- function(pathId, styleId, method, parameters, )
+{
+  token <- validString("token", token, TRUE)
+  pathId <- validUuid("pathId", pathId, TRUE)
+  styleId <- validUuid("styleId", styleId, TRUE)
+  method <- validString("method", method, TRUE)
+  parameters <- validObject("parameters", parameters, TRUE)
+  default <- validBool("default", default, FALSE)
+
+  body = list("method" = method, "parameters" = parameters, "default" = default)
+  return(apiManager_patch(glue::glue("/path/{pathId}/raster/style/{styleId}"), body, token))
+}
