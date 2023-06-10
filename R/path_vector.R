9700a478db695a1ccc7af760e3a8dfe7674927e6
B-Starkenburg <berendstarkenburg@gmail.com>
2023-06-10 22:35:22 +0200

    Added documentation to path.vector

--- R/path_vector.R
+++ R/path_vector.R
@@ -1,4 +1,11 @@
 #' @export
+#' Adds a vector
+#' @param name Mandatory (string) name for the folder
+#' @param token Mandatory (string) your token
+#' @param parentId Optional (uuid) id of folder to place new folder in
+#' @param publicAccess Optional (named list) list describing the public access of the folder
+#' @param metadata Optional (named list) list describing metadata of the folder
+#' @roxygen_header1
 path.vector.add <- function(name, token, parentId = NULL, publicAccess = NULL, metadata = NULL)
 {
   name <- validString("name", name, TRUE)
