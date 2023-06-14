4669e673cc104ea81b8f59e0c7e94c05d21c1462
B-Starkenburg <berendstarkenburg@gmail.com>
2023-06-14 12:08:30 +0200

    Added doc to path.usage

--- R/path_usage.R
+++ R/path_usage.R
@@ -1,4 +1,11 @@
 #' @export
+#' Get users that used the given path
+#' @param pathId Mandatory (uuid)
+#' @param hashtag Mandatory (string), the hashtag to add
+#' @param token Mandatory (string)
+#' @param pageStart Optional (uuid)
+#' @param listAll Optional (logical) default TRUE
+#' @roxygen_header1
 path.usage.getActiveUsers <- function(pathId, token, listAl = TRUE, pageStart = NULL)
 {
   pageStart <- validUuid("pageStart", pageStart, FALSE)
@@ -18,6 +25,11 @@ path.usage.getActiveUsers <- function(pathId, token, listAl = TRUE, pageStart = 
 }
 
 #' @export
+#' Get usage history of a specific user for a given path
+#' @param pathId Mandatory (uuid)
+#' @param userId Mandatory (uuid)
+#' @param token Mandatory (string)
+#' @roxygen_header1
 path.usage.getUsage <- function(pathId, userId, token)
 {
   pathId <- validUuid("pathId", pathId, TRUE)
@@ -29,7 +41,12 @@ path.usage.getUsage <- function(pathId, userId, token)
 }
 
 #' @export
-path.usage.getAggregatedUsage(pathId, loggedIn, token)
+#' Get aggregated usage history
+#' @param pathId Mandatory (uuid)
+#' @param token Mandatory (string)
+#' @param loggedIn Optional (logical) default TRUE
+#' @roxygen_header1
+path.usage.getAggregatedUsage(pathId, loggedIn = TRUE, token)
 {
   pathId <- validUuid("pathId", pathId, TRUE)
   loggedIn <- validBool("loggedIn", loggedIn, TRUE)
