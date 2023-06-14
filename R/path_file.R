d49285e3446ad88057a024a95a413bf95d3bcf25
B-Starkenburg <berendstarkenburg@gmail.com>
2023-06-14 12:35:32 +0200

    Added doc to path.file

--- R/path_file.R
+++ R/path_file.R
@@ -1,4 +1,9 @@
 #' @export
+#' Downloads a file
+#' @param pathId Mandatory (uuid) id of the folder
+#' @param filePath Mandatory (string) location to download file to
+#' @param token Optional (string)
+#' @roxygen_header1
 path.file.download <- function(pathId, filePath, token = NULL)
 {
   pathId <- validUuid("pathId", pathId, TRUE)
@@ -9,9 +14,20 @@ path.file.download <- function(pathId, filePath, token = NULL)
 }
 
 #' @export
+#' Adds a file
+#' @param filePath Mandatory (string) path to the file to upload
+#' @param token Mandatory (string)
+#' @param parentId Optional (uuid) id of folder to place file in
+#' @param publicAccess Optional (named list) named list desctibing public access of the file
+#' @param metadata Optional (named list) named list describing metadata of the file
+#' @roxygen_header1
 path.file.add <- function(filePath, token, parentId = NULL, publicAccess = NULL, metadata = NULL)
 {
   token <- validString("token", token, TRUE)
+  filePath <- validString("filePath", filePath, TRUE)
+  parentId <- validUuid("parentId", parentId, FALSE)
+  publicAccess <- validObject("publicAccess", publicAccess, FALSE)
+  metadata <- validObject("metadata", metadata, FALSE)
 
   fileName <- basename(filePath)
 
